ad8b2ec68741d06c2637466658c195d3ad034963
Tommy <tom@smart-homes.com>
2022-01-12 15:05:57 +0000

    Created Champion Struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
 #![allow(non_snake_case)] //Allows snake ase because I like it.
+
+struct Champion
+{
+    id : u8,
+    cost : u8,
+    
+    hp : [u8; 3],
+    sm : u8,
+    mc : u8,
+    ar : u8,
+    mr : u8,
+    ad : [u8; 3],
+    aS : u8,
+    cR : u8,
+    ra : u8,
+    
+    aID : u8,
+
+    synergies : [u8 ; 3],
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, ?!");
 }
