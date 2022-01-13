fcfae67f8a6dbd31bed57e7f75a1cb83cca7e30e
Tommy <tom@smart-homes.com>
2022-01-13 11:23:41 +0000

    Implemented and instantiated first champion, after much effort. Created summonedChampion struct

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,13 @@ struct Champion
     id : u8,
     cost : u8,
     
-    hp : [u8; 3],
+    hp : [u16; 3],
     sm : u8,
     mc : u8,
     ar : u8,
     mr : u8,
     ad : [u8; 3],
     aS : u8,
-    cR : u8,
     ra : u8,
     
     aID : u8,
@@ -20,6 +19,21 @@ struct Champion
     synergies : [u8 ; 3],
 }
 
+struct SummonedChampion
+{
+    id : u8,
+
+    star : u8,
+    items : [u8; 3],
+
+}
+
+
 fn main() {
-    println!("Hello, ?!");
+    let champions = [Champion{id : 0, cost : 1, hp : [700, 1260, 2268], sm : 0, mc : 35, ar : 25, mr : 25, ad : [75, 135, 243], aS : 7, ra : 3, aID : 0, synergies : [1, 2, 0]}, 
+                 Champion{id : 1, cost : 2, hp : [900, 1620, 2916], sm : 50, mc : 100, ar : 40, mr : 40, ad : [77, 138, 248], aS : 7, ra : 3, aID : 0, synergies : [1, 2, 0]}, 
+                 Champion{id : 2, cost : 3, hp : [700, 1260, 2268], sm : 35, mc : 35, ar : 25, mr : 25, ad : [75, 135, 243], aS : 7, ra : 3, aID : 0, synergies : [1, 2, 0]}];
+
+
+
 }
