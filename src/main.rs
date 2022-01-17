a80383f571ef71322425db07d7869db68565855e
Tommy <tom@smart-homes.com>
2022-01-17 11:35:12 +0000

    bench somethig

--- src/main.rs
+++ src/main.rs
@@ -28,12 +28,23 @@ struct SummonedChampion
 
 }
 
+struct Player
+{
+    id : u8,
+    gold : u8,
+    health : u8,
+    level : u8,
+    xp : u8,
+
+    bench : 
+
+}
 
 fn main() {
     let champions = [Champion{id : 0, cost : 1, hp : [700, 1260, 2268], sm : 0, mc : 35, ar : 25, mr : 25, ad : [75, 135, 243], aS : 7, ra : 3, aID : 0, synergies : [1, 2, 0]}, 
                  Champion{id : 1, cost : 2, hp : [900, 1620, 2916], sm : 50, mc : 100, ar : 40, mr : 40, ad : [77, 138, 248], aS : 7, ra : 3, aID : 0, synergies : [2, 3, 0]}, 
                  Champion{id : 2, cost : 3, hp : [700, 1260, 2268], sm : 35, mc : 35, ar : 25, mr : 25, ad : [75, 135, 243], aS : 7, ra : 3, aID : 0, synergies : [4, 5, 0]}];
     let mut Chadden = SummonedChampion{id : 0, star : 1, items : [0, 0, 0]};
-
+    let mut SomeGuy = SummonedChampion{id : 1, star : 2, items : [0, 0, 0]};
 
 }
