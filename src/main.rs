be67ea7ad4405762172bf4c2c46ec0a883736425
Tommy <unconfigured@null.spigotmc.org>
2022-04-24 00:34:13 +0100

    start battle

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,12 @@ struct PlacedChampion
 {
     id : u8, //champ id
 
-    star : u8, //star level
+    star : usize, //star level
     items : [u8; 3], //items given
     location : [u8; 2] //location on board
 }
 
-struct SummonedChampion
+struct Summ1dChampion
 {
 	location : [u8 ; 4],
 	health : u16,
@@ -45,12 +45,12 @@ struct SummonedChampion
 	tIDs : [[u8; 2] ; 3], //trait abilities
 }
 
-impl SummonedChampion 
+impl Summ1dChampion 
 {
 	//Method for converting PlacedChampion into SummonChampion
-	fn new(placedChampion : &PlacedChampion, ofChampion : &Champion) -> SummonedChampion
+	fn new(placedChampion : &PlacedChampion, ofChampion : &Champion) -> Summ1dChampion
 	{
-		let starLevel = placedChampion.star as usize;
+		let starLevel = placedChampion.star;
 		/*nLocation = [placedChampion.location[0], placedChampion.location[1], 0, 0];
 		
 		nHealth = ofChampion.hp[starLevel];
@@ -65,7 +65,7 @@ impl SummonedChampion
 		nAbilityID = ofChampion.aID;
 		nItems = placedChampion.items;
 		nTraits = ofChampion.traits;*/
-		SummonedChampion { location: [placedChampion.location[0], placedChampion.location[1], 0, 0],
+		Summ1dChampion { location: [placedChampion.location[0], placedChampion.location[1], 0, 0],
 						   health: ofChampion.hp[starLevel], 
 						   sm: ofChampion.sm, 
 						   dc: 0, 
@@ -85,28 +85,42 @@ impl SummonedChampion
 
 struct Player
 {
-    id : u8, //player id
+    id : u8, //p id
     gold : u8, //gold stored
     winstreak : i8, //win streak, can be +-
-    health : u8, //player health
-    level : u8, //player level
-    xp : u8, //player xp
+    health : u8, //p health
+    level : u8, //p level
+    xp : u8, //p xp
 
 
-    champions : [u8 ; 25], //all player champions
+    champions : [u8 ; 25], //all p champions
 	augments : [u8 ; 3] //augments
 }
 
 struct Board
 {
-	playerOneChampions : [PlacedChampion ; 12],
-	playerTwoChampions : [PlacedChampion ; 12],
+	p1Champions : [Summ1dChampion ; 12],
+	p2Champions : [Summ1dChampion ; 12],
 	champMS : u8, //champ movement speed in tiles per second
 	timeUnit : u8, //time unit for board in centiseconds (1/100 of a second)
 }
 
 
+impl Board
+{
+	fn new(p1PlacedChamps : [PlacedChampion ; 12], p2PlacedChamps : [PlacedChampion ; 12], champMS : u8, timeUnit : u8)
+	{
+		
+	}
+	fn StartBattle(self : Board)
+	{
+		for p1Champion in self.p1Champions
+		{
 
+		}
+
+	}
+}
 
 
 
@@ -115,7 +129,7 @@ fn main() {
     let champions = [Champion{id : 0, cost : 1, hp : [700, 1260, 2268], sm : 0, mc : 35, ar : 25, mr : 25, ad : [75, 135, 243], aS : 7, ra : 3, aID : 0, traits : [1, 2, 0]}, 
                  Champion{id : 1, cost : 2, hp : [900, 1620, 2916], sm : 50, mc : 100, ar : 40, mr : 40, ad : [77, 138, 248], aS : 7, ra : 3, aID : 0, traits : [2, 3, 0]}, 
                  Champion{id : 2, cost : 3, hp : [700, 1260, 2268], sm : 35, mc : 35, ar : 25, mr : 25, ad : [75, 135, 243], aS : 7, ra : 3, aID : 0, traits : [4, 5, 0]}];
-    let mut Chadden = SummonedChampion{id : 0, star : 1, items : [0, 0, 0]};
-    let mut SomeGuy = SummonedChampion{id : 1, star : 2, items : [0, 0, 0]};
+    //let mut Chadden = Summ1dChampion{id : 0, star : 1, items : [0, 0, 0]};
+    //let mut SomeGuy = Summ1dChampion{id : 1, star : 2, items : [0, 0, 0]};
 
 }
