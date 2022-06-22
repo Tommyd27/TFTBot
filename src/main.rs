449f2030a137a53c997bc4813c2c03b4155abf89
Tommy <tom@smart-homes.com>
2022-06-22 13:09:32 +0100

    implemented deal damage function rather than take damage, work on give item effect

--- src/main.rs
+++ src/main.rs
@@ -170,7 +170,7 @@ fn AatroxAbility(friendlyChampions : &mut Vec<SummonedChampion>, enemyChampions 
 const CHAMPIONABILITIES : [fn(friendlyChampions : &mut Vec<SummonedChampion>, enemyChampions : &mut Vec<SummonedChampion>, selfIndex : usize) ; 2]	= 
 	[LuluAbility, AatroxAbility];
 
-
+//discrepency, cast time = 0.5 seconds apparently
 
 
 
@@ -201,7 +201,7 @@ struct SummonedChampion //Structure for champions on board in battle
 	ar : i32, //armor
 	mr : i8,  //magic resist
 	ad : i32, //attack damage
-	aS : f32, //attacks per 10 seconds
+	aS : f32, //attacks per second
 	ra : u8, //auto attack range
 	aID : usize, //ability ID
 	id : usize, //id
@@ -359,6 +359,8 @@ struct Board
 }
 ///GiveItemEffect : (func)<br />
 ///Gives an item effect to a champion<br />
+///**Item IDS:**<br />
+///0 : Null<br />1  : B.F Sword (+10 Attack Damage)<br />2  : Needlessly Large Rod (+10 Ability Power)<br />3  : Giants Belt (+150 health)<br />4  : Chain Vest (+20 Armor)<br />5  : Negatron Cloak (+20 Magic Resist)<br />6  : Recurve Bow (+10% Attack Speed)<br />7  : *Sparring Gloves* (+5% Crit Chance, +10% Dodge Chance)<br />8  : Tear of the Goddess (+15 Mana)<br />9  : Spatula<br />11 : Deathblade (+40, +70, +100 Attack Damage - Star Level Dependent)<br /> 12 : *Hextech Gunblade* (Dealing Magic and True Damage heals the owner and lowest health ally for 25% of the damage)<br />13 : Zekes Herald (Grants 30% bonus attack speed to the holder and 2 adjacent allies in same row)<br />14 : Edge of Night (At 50% health - once per combat - the holder briefly becomes untargetable and sheds negative effects. Then they gain 30% attack speed)<br />15 : Bloodthirster (Damage dealt heals holder for 25%. Once per combat at 40% Health, gain a 25% maximum health shield for up to 5 seconds)<br />16 : Giant Slayer (Abilities and attacks deal 25% more damage, increased to 50% if the holder has over 2200 maximum health)<br />17 : Infinity Edge (+10 Attack Damage, +75% Crit Chance, +10% Crit Damage, Converts every 1% excess critical strike chance into 1% bonus critical strike damage)<br />18 : Spear of Shojin (✓) (Basic attacks restore an additional 8 mana on-attack)<br />19 : Shimmerscale Emblem (Wearer becomes a shimmerscale, cannot equip on a shimmersclae)<br />22 : Rabadons Deathcap (+75 Ability Power)<br />23 : Morellonomicon (+30 Ability Power, magic or true damage from an ability burns the holders target, dealing 25% of the targets maximum health as trude damage over 10 seconds and applying grevious wounds for the duration)<br />24 : Locket of the Iron Solari (At the start of combat, the wearer and all allies within 2 hexes in the same row gain a 300 / 350 / 400 health shield for 15 seconds - star level dependent)<br />25 : Ionic Spark (Enemies within 3 hexes have their magic resistance reduced by 50% (does not stack). When enemies within 3 hexes cast their ability, they are dealt 250% of their maximum mana as magic damage)<br />26 : Guinsoos Rageblade (Basic attacks grant 6% bonus attack speed for the rest of combat, stacks with no upper limit)<br />27 : *Jeweled Gauntlet* (+15% Crit Chance, +40% Crit Damage, +10 Ability Power, The holders magic adn true damage from abilities can critically strike)<br />28 : Archangels Staff (Grants the wearer 20 ability power every 5 seconds)<br />29 : Dragonmancer Emblem (Wearer becomes an dragonmancer, cannot equip on an dragonmancer)<br />33 : Warmogs Armor (+1000 Health)<br />34 : Sunfire Cape (+400 Health. At the start of combat and every 2 seconds thereafter, applies a 10% maximum health burn as true damage over 10 seconds and applying grevious wounds for the duration)<br />35 : Zephyr (At the start of combat, banishes for 5 seconds the unit that mirrors the wielders placement on the other side of the board. Pierces through CC immunity effects)<br />36 : ZZ Rot Portal (At the start of combat, the wearer taunts enemies within 4 hexes. When the wearer dies, a Voidspawn arises, taunting nearby enemies. Summoned units can spawn Voidspawns at 25% effectiveness)<br />37 : *Banshees Claw* (+15% Dodge Chance, +150 Health, At the beginning of each round, the holder and allies within 1 hex in the same row gain a shield that blocks the first enemy ability, up to 600 damage)<br />38 : Redemption (Every 5 seconds, the wearer radiates an aura to allies within 1 hex, healing them for 12% missing health. Affected allies take 25% reduced damage from AOE attacks for  seconds)<br />39 : Guardian Emblem (Wearer becomes a guardian, cannot equip on a guardian)<br />44 : Bramble Vest (+60 Armor. Negatves 75% bonus damage from critical hits. On being hit by an attack, deal 75 / 100 / 150 magic damage to all nearby enemies (once every 2.5 seconds))<br />45 : Gargoyle Stoneplate (+18 Armor and Magic Resist for each enemy targeting the holder)<br />46 : *Titans Resolve* (Gain 2 attack damage and ability power when attacking or taking damage. After stacking 25 times, gain 25 armor and magic resist and stop stacking)<br />47 : *Shroud of Stillness* (Shoot a beam that delays the first cast of affected enemies by 35%)<br />48 : Frozen Heart (Reduce the attack speed of enemies within 2 hexes by 25%)<br />49 : Cavalier Emblem (Wearer becomes a cavalier, cannot equip on a cavalier)<br />55 : Dragons Claw (+120 Magic Resist, every 2 seconds, regenerate 1.2% maximum health for each enemy targeting the holder. If holder is a dragon, increase all bonuses and effects by 20%)<br />56 : *Runaans Hurricane* (+10 Atttack Damage, attacks fire a bolt at a nearby enemy, dealing 70% of the holders attack damage as physical damage)<br />57 : *Quicksilver* (+20% attack speed. Immune to crowd control for 15 secnds)<br />58 : Chalice of Power (+30 Ability Power to holder and 2 adjacent allies on same row)<br />59 : Mirage Emblem (Wearer becomes a mirage, cannot equip on a mirage)<br />66 : Rapid Firecannon (+50% attack speed and +1 attack range, attacks cannot miss)<br />67 : *Last Whisper* (Dealing physical damage reduces the targets armor by 50% for 5 seconds, does not stack)<br />68 : Statikk Shiv (+15% attack speed, every 3rd attack shocks enemies for 70 magic damage and reduces their magic resist by 50% for 5 seconds)<br />69 : Ragewing Emblem (Wearer becomes a ragewing, cannot equip on a ragewing)<br />77 : *Thiefs Gloves* (Each round equip 2 random items, improve with player level, you cannot equip other items)<br />78 : *Hand of Justice* (+15 attack damage, +15% ability power. Attacks and abilities heal for 15% of damage dealt. Each round randomly increase 1 effect by 30%)<br />79 : *Assassin Emblem* (Wearer becomes an assassin, cannot equip on an assassin)<br />88 : Blue Buff (+20 Starting Mana. Gain 20 mana after casting an ability)<br />89 : Mage Emblem (Wearer becomes a mage, cannot equip on a mage)<br />99 : Tacticians Crown (Increase board unit size by 1)<br />
 fn GiveItemEffect(item : u8, champion : &mut SummonedChampion)
 {
 	if item == 0 
@@ -367,6 +369,15 @@ fn GiveItemEffect(item : u8, champion : &mut SummonedChampion)
 	}
 	match item
 	{
+		1 | 12 | 18 => champion.ad += 10,
+		2 | 12 => champion.ap += 10,
+		3 => champion.health += 150,
+		4 => champion.ar += 20,
+		5 => champion.mr += 20,
+		6 => champion.attackSpeedModifier += 0.1,
+		7 => {champion.cr += 5; champion.dc += 10},
+		8  | 18 => champion.cm += 15,
+		11 => champion.ad += [40, 70, 100][champion.starLevel],
 		_ => println!("Unimplemented Item"),
 	}
 }
@@ -495,25 +506,34 @@ fn sign(num : i8) -> i8
 		-1
 	}
 }
-
-/*fn InGridHexagon(pos : [i8 ; 2], gridSize : [i8 ; 3]) -> bool//need to check for correct gridsize
+///damageType : 0 = physical, 1 = magical, 2 = true
+fn dealDamage(selfIndex : usize,
+			  friendlyChampions : &mut Vec<SummonedChampion>,
+			  target : &mut SummonedChampion,
+			  damageAmount : i32,
+			  damageType : u8,
+			  )
 {
-	if pos[0] >= 0 && pos[0] < gridSize[0] &&
-	   pos[1] >= 0 && pos[1] < gridSize[1]
+	let mut damage : i32 = 0;
+	match damageType
 	{
-		if gridSize[2] == 1 //optimisation
-		{
-			if 2 - (pos[1] / 2) < pos[0] && //doesnt work for different grid sizes has to be changed manually
-			   10 - (pos[1] / 2) > pos[0]
-			{
-				return true
-			}
-			return false
-		}
-		return true
+		0 => {damage = (100 * damageAmount * target.incomingDMGModifier) / (100 * (100 + target.ar));
+			  if friendlyChampions[selfIndex].cr > rand::thread_rng().gen_range(0..100)
+			  {
+				damage *= 13;
+				damage /= 10;
+			  }
+
+		},
+		1 => {damage = (100 * damageAmount * friendlyChampions[selfIndex].ap * target.incomingDMGModifier) / (100 * (100 + target.mr))},
+		2 => {},
 	}
-	return false
-}*/
+	if target.gMD <= 0
+	{
+			target.cm += (7 * damage  / 100) as u8; //discrepency, should be 1% of premitigation and 7% of post.
+	}
+}
+
 
 fn InGridHexagon(pos : [i8 ; 2]) -> bool//not going to attempt getting it working for different grid sizes yet
 {
@@ -644,6 +664,10 @@ fn takeTurn(selfIndex : usize, friendlyChampions : &mut Vec<SummonedChampion>, e
 			if friendlyChampions[selfIndex].gMD <= 0
 			{
 				friendlyChampions[selfIndex].cm += 10;
+				if friendlyChampions[selfIndex].items.contains(&18)
+				{
+					friendlyChampions[selfIndex].cm += 8;
+				}
 				println!("gain mana");
 			}
 			println!("maybe dodge");
