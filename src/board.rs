a0515ae4ebc68bf1e91990d551a658594d51dcf3
Tom JP Laptop <tom@smart-homes.com>
2022-12-14 15:05:34 +0000

    bye

--- src/board.rs
+++ src/board.rs
@@ -27,9 +27,8 @@ impl Board {
         let mut p1Champions = VecDeque::new();
         let mut p2Champions = VecDeque::new();
         let len: usize = p1PlacedChamps.len();
-        for (i, p1Champion) in p1PlacedChamps.iter().enumerate()
-        //(!O) converts placed champions to summoned champions
-        {
+        for (i, p1Champion) in p1PlacedChamps.iter().enumerate() {
+            //(!O) converts placed champions to summoned champions
             p1Champions.push_back(SummonedChampion::new(p1Champion, i)); //converts into summoned champ
         }
 
@@ -41,7 +40,7 @@ impl Board {
             p1Champions,
             p2Champions,
             timeUnit,
-            movementAmount: 10 / timeUnit as i8, //(!O)
+            movementAmount: 10 / (timeUnit as i8), //(!O)
         } //creates new board
     }
 
@@ -55,9 +54,8 @@ impl Board {
         {
             println!("Debug : Iteration {}", debugCount);
             debugCount += 1; //count turns
-            for _champCount in 0..self.p1Champions.len()
-            //take turn for all p1Champs
-            {
+            for _champCount in 0..self.p1Champions.len() {
+                //take turn for all p1Champs
                 let mut thisChamp = self.p1Champions.pop_front().unwrap();
                 thisChamp.setup(&mut self.p1Champions, &mut self.p2Champions);
                 let alive = thisChamp.takeTurn(
@@ -68,15 +66,14 @@ impl Board {
                     &mut p1Projectiles,
                 );
                 if alive {
-                    self.p1Champions.push_back(thisChamp)
+                    self.p1Champions.push_back(thisChamp);
                 } else {
                     deadChamps.push_back(thisChamp);
                 }
             }
 
-            for _champCount in 0..self.p2Champions.len()
-            //take turn for all p1Champs
-            {
+            for _champCount in 0..self.p2Champions.len() {
+                //take turn for all p1Champs
                 let mut thisChamp = self.p2Champions.pop_front().unwrap();
                 thisChamp.setup(&mut self.p2Champions, &mut self.p1Champions);
                 let alive = thisChamp.takeTurn(
@@ -87,9 +84,9 @@ impl Board {
                     &mut p2Projectiles,
                 );
                 if alive {
-                    self.p2Champions.push_back(thisChamp)
+                    self.p2Champions.push_back(thisChamp);
                 } else {
-                    deadChamps.push_back(thisChamp)
+                    deadChamps.push_back(thisChamp);
                 }
             }
             p1Projectiles.retain_mut(|f| {
@@ -107,20 +104,11 @@ impl Board {
                 )
             }); //simulate projectile ticks
         }
-        println!("Debug : Battle Over");
         if self.p1Champions.is_empty()
         //check winner and get champ information
         {
-            println!("Debug : Player 2 Won");
-            for champion in &self.p2Champions {
-                println!("Champ Remaining {champion}")
-            }
             2
         } else {
-            println!("Debug : Player 1 Won");
-            for champion in &self.p1Champions {
-                println!("Champ Remaining {champion}")
-            }
             1
         }
     }
