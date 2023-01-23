4e8f64b0dc2398b471a1715aa15865cfa5e032c5
Tommy <unconfigured@null.spigotmc.org>
2023-01-23 22:37:05 +0000

    need to make frotnednw ork baby

--- src/routes/change_stats/+page.svelte
+++ src/routes/change_stats/+page.svelte
@@ -1,1 +1,31 @@
+<script>
+    import { invoke } from "@tauri-apps/api/tauri"
+
+    async function fetch_champs() {
+        console.log(await invoke("retrieve_all_unit_ids"))
+        return await invoke("retrieve_all_unit_ids")
+    }
+    async function fetch_items() {
+        return await invoke("retrieve_all_item_ids")
+    }
+
+    let champs = fetch_champs()
+    let items = fetch_items()
+</script>
+
+{#await champs}
+    <p>...fetching champs</p>
+{:then}
+    <p></p>
+{:catch error}
+    <p>Failure to fetch champs {error}</p>
+{/await}
+
+{#await items}
+    <p>...fetching items</p>
+{:then}
+
+{:catch error}
+    <p>Failure to fetch items {error}</p>
+{/await}
 <h1>Change Stats</h1>
